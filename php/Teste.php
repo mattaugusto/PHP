8b093e2d78b02d2524d30208690f28a4f471d8c2
mattaugusto <matheus.augusto.jp@gmail.com>
2017-10-04 10:22:03 -0300

    atualizando

--- php/Teste.php
+++ php/Teste.php
@@ -8,9 +8,46 @@
 
 class Teste
 {
+    public $nome;
+    public $cpf;
 
+    public function __construct(string $nome, string $cpf)
+    {
+        $this->nome = $nome;
+        $this->cpf = $cpf;
 
+        return $this;
+    }
 
+    public function set_Nome(string $nome)
+    {
+        $this->nome = $nome;
+    }
 
+    public function get_Nome():string
+    {
+         return $this->nome;
+    }
 
+    public function set_cpf(string $cpf)
+    {
+        $this->cpf = $cpf;
+    }
+
+    public function get_cpf(string $cpf):string
+    {
+        return $this->cpf;
+    }
+
+    public function __toString():string
+    {
+        return "$this->nome - $this->cpf";
+    }
 }
+
+$teste = new Teste('João','123123123');
+
+echo $teste->get_Nome();
+
+
+
